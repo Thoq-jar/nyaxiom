161efed16f51220f5e635f997ba9eca40fcef574
Thoq <tristanshaw000@gmail.com>
2026-05-13 15:51:38 -0400

    nicer installs

--- install.py
+++ install.py
@@ -1,32 +1,81 @@
 #!/usr/bin/env python3
 
-import subprocess
 from pathlib import Path
+from subprocess import CompletedProcess
 
-DEPS = ["python3.13", "git"]
-NAME = "axiom"
-PATH = "/home/axiom"
+DEPS: list[str] = ["python3.13", "git"]
+NAME: str = "axiom"
+PATH: str = "/home/axiom"
+BANNER: str = """
+⠀⠀⠀⠀⢐⡝⠛⢦⠀⠀⠘⠛⢦⡀⢀⣀⢀⣴⡯⠓⠀⣀⠔⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣠⠚⠁⠀⠀⠑⣣⢶⣿⣶⣿⣿⠿⢿⣥⣶⡛⠛⠰⢠⣀⠀⠀⠀⠀⠀⠀
+⠀⠀⠈⠉⠀⣷⣶⡤⠞⣵⡿⢛⡼⠛⣭⢟⣆⠙⢿⡛⣿⣢⣀⠘⠷⣄⢀⣠⣤⠀
+⠀⠀⠀⠀⠤⢾⡟⢁⡜⠁⠠⠍⠀⠀⡈⠈⠚⣂⠈⢷⢌⣯⢻⡝⣳⣼⣯⢽⡟⠀
+⠀⠀⠀⠀⣠⡞⣴⠊⡄⢠⡏⠀⠀⡄⣇⢆⠀⠈⢧⡈⢧⣻⡷⣽⣾⣿⣷⣼⣦⠀
+⠀⠀⣴⣾⣿⣭⡇⢼⣰⣯⣿⠀⠀⡇⣿⡘⣆⢰⣾⣿⣦⣻⣿⣮⣿⣿⣿⣿⣿⠀
+⠀⠠⠿⣻⢿⣿⡃⢰⠿⣿⢹⣆⢀⣿⠋⣷⠻⣷⡁⠺⣿⣧⣷⡶⠻⢻⣽⣟⠃⠱
+⠀⠀⢰⣿⢿⣟⣍⢏⠾⢸⣶⠉⢸⡿⣸⠈⢻⣵⣷⣄⡛⡎⢪⣯⣳⣌⢹⡇⠀⢀
+⠀⢀⣿⣵⡿⢿⣼⣟⢻⣷⣾⣷⡀⣧⢹⡀⠀⠙⠳⡿⣷⢱⢸⢏⢿⡿⣮⡀⠀⠈
+⠀⢨⣿⣿⣹⡎⡷⣏⠸⣿⠏⠈⠻⣿⠆⠷⠾⠿⡶⣶⡞⣼⢸⣌⠻⢿⣿⣄⡶⠠
+⠀⣾⢿⡿⢻⣇⡇⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡟⣽⡇⡇⢫⣧⣸⣿⠟⠿⣆
+⠘⠃⠈⠁⠈⣽⢱⣈⢱⣄⠀⠶⠒⠂⡄⠀⣠⢾⣿⣣⡏⣛⣹⣦⣿⣿⣻⢿⣆⠀
+⠀⠀⠀⠀⣀⣽⡶⠿⠍⠘⢻⣆⣘⣋⣤⠴⢛⣵⡿⢻⣷⡿⣵⠀⣿⣿⠟⠀⠙⠀
+⢀⡲⡼⠏⠉⣾⠇⠀⠀⣠⡞⢁⣿⡿⣾⣿⠾⢟⡿⢸⣿⠀⠙⣧⣾⠀⠀⠀⠀⠀
+⠬⠥⠤⠐⣲⣿⣆⠀⠀⠸⣄⡬⠙⣁⢽⠙⠦⣩⣅⠀⠱⣄⠞⠤⠩⠄⠀⠀⠀⠀
+"""
+
+
+class Ascii:
+    PINK = "\x1b[38;5;206m"
+    BOLD = "\x1b[1m"
+    RESET = "\x1b[0m"
+
+
+print(Ascii.PINK + BANNER + Ascii.RESET)
+
+
+def log(message: str) -> None:
+    print(f"{Ascii.BOLD}{Ascii.PINK}AXIOM:{Ascii.RESET} {message}")
+
+
+def run(
+    cmd: list, capture_output: bool = False, text: bool = False, check: bool = False
+) -> CompletedProcess:
+    import subprocess
+
+    log(f"running: {cmd}")
+    try:
+        return subprocess.run(
+            cmd, check=check, capture_output=capture_output, text=text
+        )
+    except subprocess.CalledProcessError as exception:
+        return subprocess.CompletedProcess(
+            cmd, returncode=exception.returncode, stdout=None, stderr=None
+        )
 
 
 def check_command(command: str) -> bool:
-    return subprocess.run([command, "--version"], capture_output=True).returncode == 0
+    log(f"checking for {command}")
+    return run([command, "--version"], capture_output=True).returncode == 0
 
 
 def find_exe(command: str) -> str:
-    result = subprocess.run(["which", command], capture_output=True, text=True)
+    log(f"finding {command}")
+    result = run(["which", command], capture_output=True, text=True)
     return result.stdout.strip()
 
 
 def check_deps() -> None:
     for dep in DEPS:
         if not check_command(dep):
-            print(f"Error: {dep} is not installed")
+            log(f"error: {dep} is not installed")
             exit(1)
         else:
-            print(f"Found: {dep} ({find_exe(dep)})")
+            log(f"found: {dep} ({find_exe(dep)})")
 
 
 def parse_os_release(content: str, key: str) -> str:
+    log(f"parsing os release for {key}")
     for line in content.splitlines():
         if line.startswith(key + "="):
             return line.split("=")[1].strip()
@@ -34,6 +83,7 @@ def parse_os_release(content: str, key: str) -> str:
 
 
 def get_os_release() -> str:
+    log("getting os release")
     release = Path("/etc/os-release")
     with open(release, "r") as file:
         content = file.read()
@@ -45,83 +95,80 @@ def get_os_release() -> str:
 
 
 def check_os() -> None:
+    log("checking OS")
     operating_system = get_os_release()
-    print(f"Detected OS: {operating_system}")
+    log(f"detected OS: {operating_system}")
     if operating_system != "arch":
-        print(
-            "Unsupported OS: only Arch Linux is supported at the moment, Debian is planned."
+        log(
+            "unsupported OS: only Arch Linux is supported at the moment, Debian is planned."
         )
         exit(1)
     else:
-        print("     > OS is supported")
+        log("     > OS is supported")
 
 
 def add_user(username: str, password: str):
-    print(f"Creating user '{username}' with password '{password}'...")
+    log(f"creating user '{username}' with password '{password}'...")
 
     try:
-        subprocess.run(["sudo", "useradd", "-p", password, username])
+        run(["sudo", "useradd", "-p", password, username])
     except Exception as exception:
-        print(f"    > Failed to add user: {exception}")
+        log(f"    > failed to add user: {exception}")
         exit(1)
-    print(f"     > User '{username}' created successfully")
+    log(f"     > user '{username}' created successfully")
 
 
 def setup_user_stage1() -> None:
     import getpass
 
+    log("setting up user stage 1")
     current_user = getpass.getuser()
-    print(f"Assigning {PATH} to {current_user} for installation...")
+    log(f"assigning {PATH} to {current_user} for installation...")
 
-    subprocess.run(["sudo", "mkdir", "-p", PATH], check=True)
-    subprocess.run(
-        ["sudo", "chown", "-R", f"{current_user}:{current_user}", PATH], check=True
-    )
+    run(["sudo", "mkdir", "-p", PATH], check=True)
+    run(["sudo", "chown", "-R", f"{current_user}:{current_user}", PATH], check=True)
 
 
 def setup_user_stage2() -> None:
     import glob
     import os
 
-    subprocess.run(["sudo", "chown", "-R", f"{NAME}:{NAME}", PATH], check=True)
-    subprocess.run(["sudo", "chmod", "755", PATH], check=True)
+    log("setting up user stage 2")
+    run(["sudo", "chown", "-R", f"{NAME}:{NAME}", PATH], check=True)
+    run(["sudo", "chmod", "755", PATH], check=True)
     bin_path = f"{PATH}/bin"
     venv_bin = f"{PATH}/.venv/bin"
-    subprocess.run(["sudo", "chmod", "-R", "755", bin_path], check=True)
+    run(["sudo", "chmod", "-R", "755", bin_path], check=True)
     for file in glob.glob(f"{bin_path}/*"):
-        subprocess.run(["sudo", "chmod", "+x", file], check=True)
+        run(["sudo", "chmod", "+x", file], check=True)
     if os.path.exists(venv_bin):
-        subprocess.run(["sudo", "chmod", "-R", "755", venv_bin], check=True)
+        run(["sudo", "chmod", "-R", "755", venv_bin], check=True)
 
 
 def clone_repo() -> None:
     import os
 
+    log("cloning repo")
     repo_url = "https://github.com/Thoq-jar/nyaxiom.git"
-    subprocess.run(
-        ["git", "config", "--global", "--add", "safe.directory", PATH], check=True
-    )
+    run(["git", "config", "--global", "--add", "safe.directory", PATH], check=True)
     if os.path.exists(os.path.join(PATH, ".git")):
-        subprocess.run(["git", "-C", PATH, "pull"], check=True)
+        run(["git", "-C", PATH, "pull"], check=True)
     else:
-        subprocess.run(["git", "clone", repo_url, PATH], check=True)
+        run(["git", "clone", repo_url, PATH], check=True)
 
 
 def setup_venv() -> None:
     python = find_exe("python3.13") or "/usr/bin/python3.13"
-    subprocess.run(["sudo", "chown", "-R", f"{NAME}:{NAME}", PATH], check=True)
-    subprocess.run(
-        ["sudo", "-u", NAME, python, "-m", "venv", f"{PATH}/.venv"], check=True
-    )
-    subprocess.run(
-        ["sudo", "-u", NAME, f"{PATH}/.venv/bin/pip", "install", PATH], check=True
-    )
+    log("setting up venv")
+    run(["sudo", "chown", "-R", f"{NAME}:{NAME}", PATH], check=True)
+    run(["sudo", "-u", NAME, python, "-m", "venv", f"{PATH}/.venv"], check=True)
+    run(["sudo", "-u", NAME, f"{PATH}/.venv/bin/pip", "install", PATH], check=True)
 
 
 def register_service() -> None:
     import subprocess
 
-    print("Registering service...")
+    log("registering service...")
     subprocess.run(
         [
             "sudo",
@@ -132,7 +179,7 @@ def register_service() -> None:
         check=True,
     )
     subprocess.run(["sudo", "systemctl", "daemon-reload"], check=True)
-    print("     > Service registered successfully")
+    log("     > service registered successfully")
 
 
 def main() -> None:
@@ -145,11 +192,11 @@ def main() -> None:
     setup_user_stage2()
     register_service()
 
-    print("Installation complete!")
-    print(" | To start Axiom, run: `sudo systemctl start axiomd`")
-    print(" | To enable Axiom on boot, run: `sudo systemctl enable axiomd`")
-    print(" | To do both, run: `sudo systemctl enable --now axiomd`")
-    print(" | If you just updated, run: `sudo systemctl restart axiomd`")
+    log("Installation complete!")
+    log(" | To start Axiom, run: `sudo systemctl start axiomd`")
+    log(" | To enable Axiom on boot, run: `sudo systemctl enable axiomd`")
+    log(" | To do both, run: `sudo systemctl enable --now axiomd`")
+    log(" | If you just updated, run: `sudo systemctl restart axiomd`")
 
 
 if __name__ == "__main__":
--- uninstall.py
+++ uninstall.py
@@ -8,7 +8,41 @@ PATH = "/home/axiom"
 SERVICE_PATH = "/etc/systemd/system/axiomd.service"
 
 
+BANNER: str = """
+⠀⠀⠀⠀⢐⡝⠛⢦⠀⠀⠘⠛⢦⡀⢀⣀⢀⣴⡯⠓⠀⣀⠔⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣠⠚⠁⠀⠀⠑⣣⢶⣿⣶⣿⣿⠿⢿⣥⣶⡛⠛⠰⢠⣀⠀⠀⠀⠀⠀⠀
+⠀⠀⠈⠉⠀⣷⣶⡤⠞⣵⡿⢛⡼⠛⣭⢟⣆⠙⢿⡛⣿⣢⣀⠘⠷⣄⢀⣠⣤⠀
+⠀⠀⠀⠀⠤⢾⡟⢁⡜⠁⠠⠍⠀⠀⡈⠈⠚⣂⠈⢷⢌⣯⢻⡝⣳⣼⣯⢽⡟⠀
+⠀⠀⠀⠀⣠⡞⣴⠊⡄⢠⡏⠀⠀⡄⣇⢆⠀⠈⢧⡈⢧⣻⡷⣽⣾⣿⣷⣼⣦⠀
+⠀⠀⣴⣾⣿⣭⡇⢼⣰⣯⣿⠀⠀⡇⣿⡘⣆⢰⣾⣿⣦⣻⣿⣮⣿⣿⣿⣿⣿⠀
+⠀⠠⠿⣻⢿⣿⡃⢰⠿⣿⢹⣆⢀⣿⠋⣷⠻⣷⡁⠺⣿⣧⣷⡶⠻⢻⣽⣟⠃⠱
+⠀⠀⢰⣿⢿⣟⣍⢏⠾⢸⣶⠉⢸⡿⣸⠈⢻⣵⣷⣄⡛⡎⢪⣯⣳⣌⢹⡇⠀⢀
+⠀⢀⣿⣵⡿⢿⣼⣟⢻⣷⣾⣷⡀⣧⢹⡀⠀⠙⠳⡿⣷⢱⢸⢏⢿⡿⣮⡀⠀⠈
+⠀⢨⣿⣿⣹⡎⡷⣏⠸⣿⠏⠈⠻⣿⠆⠷⠾⠿⡶⣶⡞⣼⢸⣌⠻⢿⣿⣄⡶⠠
+⠀⣾⢿⡿⢻⣇⡇⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡟⣽⡇⡇⢫⣧⣸⣿⠟⠿⣆
+⠘⠃⠈⠁⠈⣽⢱⣈⢱⣄⠀⠶⠒⠂⡄⠀⣠⢾⣿⣣⡏⣛⣹⣦⣿⣿⣻⢿⣆⠀
+⠀⠀⠀⠀⣀⣽⡶⠿⠍⠘⢻⣆⣘⣋⣤⠴⢛⣵⡿⢻⣷⡿⣵⠀⣿⣿⠟⠀⠙⠀
+⢀⡲⡼⠏⠉⣾⠇⠀⠀⣠⡞⢁⣿⡿⣾⣿⠾⢟⡿⢸⣿⠀⠙⣧⣾⠀⠀⠀⠀⠀
+⠬⠥⠤⠐⣲⣿⣆⠀⠀⠸⣄⡬⠙⣁⢽⠙⠦⣩⣅⠀⠱⣄⠞⠤⠩⠄⠀⠀⠀⠀
+"""
+
+
+class Ascii:
+    PINK = "\x1b[38;5;206m"
+    BOLD = "\x1b[1m"
+    RESET = "\x1b[0m"
+
+
+print(Ascii.PINK + BANNER + Ascii.RESET)
+
+
+def log(message: str) -> None:
+    print(f"{Ascii.BOLD}{Ascii.PINK}AXIOM:{Ascii.RESET} {message}")
+
+
 def run(cmd: list):
+    log(f"running: {cmd}")
+
     try:
         subprocess.run(cmd, check=True, capture_output=True)
     except subprocess.CalledProcessError:
@@ -16,22 +50,22 @@ def run(cmd: list):
 
 
 def uninstall() -> None:
-    print(f"Stopping and disabling {NAME}d service...")
+    log("stopping and disabling axiomd service...")
     run(["sudo", "systemctl", "stop", "axiomd"])
     run(["sudo", "systemctl", "disable", "axiomd"])
 
     if Path(SERVICE_PATH).exists():
-        print("Removing service file...")
+        log("removing service file...")
         run(["sudo", "rm", SERVICE_PATH])
         run(["sudo", "systemctl", "daemon-reload"])
 
-    print("Removing user and home directory...")
+    log("removing user and home directory...")
     run(["sudo", "userdel", "-r", NAME])
 
-    print("Cleaning git safe directory config...")
+    log("cleaning git safe directory config...")
     run(["sudo", "git", "config", "--global", "--unset-all", "safe.directory", PATH])
 
-    print("Uninstall complete!")
+    log("uninstall complete!")
 
 
 if __name__ == "__main__":
